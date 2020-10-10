7e6454a1f77ae0b501f5c72cc231392793ff4ae5
sheldonl3 <tssheldonpotter@gmail.com>
2020-10-10 09:32:46 +0800

    test goland push

--- algorithm/main.go
+++ algorithm/main.go
@@ -8,7 +8,7 @@ func ScanLine() string { //输入一行字符串，包括空格
 	var c byte
 	var err error
 	var b []byte
-	for ; err == nil; {
+	for err == nil {
 		_, err = fmt.Scanf("%c", &c)
 		if c != '\n' {
 			b = append(b, c)
@@ -27,5 +27,5 @@ func main() {
 	//	fmt.Println(zhuangzai(s))
 	//}
 
-	fmt.Println(addStrings("1213","456"))
+	fmt.Println(addStrings("1213", "56"))
 }
