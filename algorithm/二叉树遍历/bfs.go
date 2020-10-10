9fd3aa23eddf413e065673d7e7d407b027f5bdc5
sheldonl3 <tssheldonpotter@gmail.com>
2020-10-10 15:07:54 +0800

    二叉树遍历

---
+++ algorithm/二叉树遍历/bfs.go
@@ -0,0 +1,100 @@
+package tree_travel
+
+//层序遍历
+func cengxu(p *TreeNode) []int {
+	res := make([]int, 0)
+	if p == nil {
+		return res
+	}
+	queue := []*TreeNode{p}
+	for len(queue) > 0 {
+		length := len(queue)
+		for length > 0 {
+			length--
+			if queue[0].Left != nil {
+				queue = append(queue, queue[0].Left)
+			}
+			if queue[0].Right != nil {
+				queue = append(queue, queue[0].Right)
+			}
+			res = append(res, queue[0].Val)
+			queue = queue[1:]
+		}
+	}
+	return res
+}
+
+//先序遍历
+func preorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+
+	for len(queue) > 0 || root != nil {
+		for root != nil {
+			result = append(result, root.Val)
+			queue = append(queue, root)
+			root = root.Left
+		}
+		root = queue[len(queue)-1].Right
+		queue = queue[:len(queue)-1]
+	}
+	return result
+}
+
+//中序遍历
+func inorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+
+	for len(queue) > 0 || root != nil {
+		for root != nil {
+			queue = append(queue, root)
+			root = root.Left
+		}
+
+		node := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+		result = append(result, node.Val)
+		root = node.Right
+	}
+	return result
+}
+
+// 后序遍历
+func postorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+
+	if root == nil {
+		return result
+	}
+
+	queue := make([]*TreeNode, 0)
+	var lastVisited *TreeNode
+
+	for len(queue) > 0 || root != nil {
+		for root != nil {
+			queue = append(queue, root)
+			root = root.Left
+		}
+		n := queue[len(queue)-1]
+		if n.Right == nil || n.Right == lastVisited {
+			result = append(result, n.Val)
+			queue = queue[:len(queue)-1]
+			lastVisited = n
+		} else {
+			root = n.Right
+		}
+	}
+
+	return result
+}
---
+++ algorithm/二叉树遍历/dfs.go
@@ -0,0 +1,34 @@
+package tree_travel
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//先序遍历
+func preorder(p *TreeNode, res *[]int) {
+	if p != nil {
+		*res = append(*res, p.Val)
+		preorder(p.Left, res)
+		preorder(p.Right, res)
+	}
+}
+
+//中序遍历
+func inorder(p *TreeNode, res *[]int) {
+	if p != nil {
+		inorder(p.Left, res)
+		*res = append(*res, p.Val)
+		inorder(p.Right, res)
+	}
+}
+
+//后序遍历
+func postorder(p *TreeNode, res *[]int) {
+	if p != nil {
+		postorder(p.Left, res)
+		postorder(p.Right, res)
+		*res = append(*res, p.Val)
+	}
+}
